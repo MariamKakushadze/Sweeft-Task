543c7a61d7d8f87c014152e5209730f56f319e15
MariamK <102153226+MariamKakushadze@users.noreply.github.com>
2023-03-16 15:52:03 +0400

    feat: create header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,8 @@
+const Header = () => {
+  return (
+    <div className="flex justify-center items-center p-5">
+      <h1 className=" text-red-800 text-lg">List of Users</h1>
+    </div>
+  );
+};
+export default Header;
