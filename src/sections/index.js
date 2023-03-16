8ea83615105e4b4481ce0f48ab5901417a1cd9f1
MariamK <102153226+MariamKakushadze@users.noreply.github.com>
2023-03-16 22:13:02 +0400

    feat: change name for userinfo component

--- src/sections/index.js
+++ src/sections/index.js
@@ -1,2 +1,3 @@
 export { default as List } from "sections/UserList/UserList";
 export { default as FiendsList } from "sections/FriendsList/FriendsList";
+export { default as UserInfo } from "sections/UserInfo/UserInfo";
