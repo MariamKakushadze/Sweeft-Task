8074b09b611a082db0e578fb25830eab0a7c7429
MariamK <102153226+MariamKakushadze@users.noreply.github.com>
2023-03-16 22:53:11 +0400

    style: increase font for bigger screen

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,9 @@
 const Header = () => {
   return (
-    <div className="flex justify-center items-center p-5">
-      <h1 className=" text-red-800 text-lg">List of Users</h1>
+    <div className="flex justify-center items-center p-5 lg:p-8">
+      <h1 className=" text-red-800 text-lg  lg:font-bold lg:text-2xl ">
+        List of Users
+      </h1>
     </div>
   );
 };
