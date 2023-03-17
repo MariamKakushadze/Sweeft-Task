3e491a305d425d185ed30f9084e5761f20bf68b3
MariamK <102153226+MariamKakushadze@users.noreply.github.com>
2023-03-17 15:20:05 +0400

    feat: scroll up after clicking card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,8 +1,17 @@
+import { useNavigate } from "react-router-dom";
+
 const Card = ({ id, name, lastName, prefix, title, imageUrl }) => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate("/user/:id");
+    window.scrollTo(0, 0);
+  };
   return (
     <div
       className="text-xs font-light flex flex-col gap-1
       border rounded-md pb-2 sm:max-w-[37.5 rem] min-w-[18.5 rem] cursor-pointer hover:scale-110"
+      onClick={handleClick}
     >
       <img src={`${imageUrl}?${id}`} alt={`${name} ${lastName}`} />
       <p className="font-normal  px-2">
